af34c1d9c0496bbee29bd5690df02da811c6c805
Pedro Henrique Passos Carreira <peagacarreira@gmail.com>
2024-10-15 15:04:09 -0300

    pulamos

--- Assassin's Chris/Assets/Scripts/PlayerController.cs
+++ Assassin's Chris/Assets/Scripts/PlayerController.cs
@@ -11,12 +11,13 @@ public class PlayerController : MonoBehaviour
     [Header("Player Movement")]
     [SerializeField] private float playerSpeed;             //velocidade em que o jogador se move
     private Vector2 movementInput;                          //entrada de movimento
-    private Vector2 movementDirection;                      //qual direçao x e y o jogador ira andar
-    [SerializeField] private float jumpForce = 10;
+    private Vector2 movementDirection;                      //qual direçao jogador ira andar
+    [SerializeField] private float jumpForce;
     
     [Header("Player Limits")]
     [SerializeField] private float maxX;
     [SerializeField] private float minX;
+    [SerializeField] private float maxY;
     
     void Start()
     {
@@ -25,24 +26,20 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        ReceiveInputs();
         PlayerMovement();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            oRigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            oRigidbody2D.velocity = new Vector2(oRigidbody2D.velocity.x, jumpForce);
         }
     }
     
-    private void ReceiveInputs()
-    {
-        // Armazena a direção que o jogador define
-        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), oRigidbody2D.velocity.y);
-    }
     private void PlayerMovement()
     {
-        //movimenta o jogador com base na direçao
-        oRigidbody2D.velocity = movementInput * playerSpeed;
+        // Armazena a direção que o jogador define
+        oRigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * playerSpeed, oRigidbody2D.velocity.y);
+        
+        
 
         //limita movimentaçao d player
         oRigidbody2D.position = new Vector2(Mathf.Clamp(oRigidbody2D.position.x, minX, maxX), oRigidbody2D.position.y);
