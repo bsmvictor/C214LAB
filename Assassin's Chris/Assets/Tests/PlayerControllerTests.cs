dd2163144ab92afb15dd9c930368f061a725adb9
Felipe Conrado <fconrado2003@gmail.com>
2024-10-18 11:58:46 -0300

    mudando as funcoes

--- Assassin's Chris/Assets/Tests/PlayerControllerTests.cs
+++ Assassin's Chris/Assets/Tests/PlayerControllerTests.cs
@@ -18,7 +18,11 @@ public class PlayerControllerTests
         playerController = player.AddComponent<PlayerController>();
         oRigidbody2D = player.AddComponent<Rigidbody2D>();
 
+<<<<<<< HEAD
         // Configura os parÃ¢metros do PlayerController
+=======
+        // Configura os parâmetros do PlayerController
+>>>>>>> ccf414a2d969e0aaad67b5274c58fa2ce585e6fd
         playerController.SetPlayerSpeed(5f);
         playerController.SetMinX(-10f);
         playerController.SetMaxX(10f);
@@ -39,4 +43,8 @@ public class PlayerControllerTests
     //    Assert.Greater(player.transform.position.y, initialYPosition);
     //    Assert.AreEqual(jumpForce, oRigidbody2D.velocity.y);
     //}
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> ccf414a2d969e0aaad67b5274c58fa2ce585e6fd
